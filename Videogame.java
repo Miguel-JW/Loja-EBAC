dcdd020a6bbbe852f2c092bff4aae9c8b97cdcf0
Miguel-JW <conteudojurassicojw@gmail.com>
2026-05-27 08:37:49 -0300

    Create Videogame.java

---
+++ Videogame.java
@@ -0,0 +1,6 @@
+public class Videogame extends Produto {
+    public Videogame() { super("Videogame", 4800.00); }
+
+    @Override
+    public String descricao() { return "Console de última geração"; }
+}
