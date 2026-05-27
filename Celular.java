a37e5b01f5905e805e57b38f7d8cb539ff9ee4af
Miguel-JW <conteudojurassicojw@gmail.com>
2026-05-27 08:39:12 -0300

    Create Celular.java

---
+++ Celular.java
@@ -0,0 +1,6 @@
+public class Celular extends Produto {
+    public Celular() { super("Celular", 3200.00); }
+
+    @Override
+    public String descricao() { return "Smartphone 256GB câmera 108MP"; }
+}
